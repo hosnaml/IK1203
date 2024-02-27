e0904a38a48d8444a52175c30331fb25ab4d01c2
hosnaml <hosna@kth.se>
2024-02-27 18:22:41 +0100

    Final one doing bad request

--- task3/submission/TCPClient.java
+++ task3/submission/TCPClient.java
@@ -14,54 +14,56 @@ public class TCPClient {
         this.LIMIT = limit;
     }
 
-    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
+    public byte[] askServer(String hostname, int port, byte[] toServerBytes)
+            throws IOException, SocketTimeoutException {
 
-        //use dynamic output stream
+        // use dynamic output stream
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         // Pre-allocate byte buffers for receiving
         byte[] fromServerBuffer = new byte[BUFFERSIZE];
 
         // TODO: Error handling
 
-        try(Socket clientSocket = new Socket(hostname, port)) {
+        Socket clientSocket = new Socket(hostname, port);
+        // try(Socket clientSocket = new Socket(hostname, port)) {
 
-            //If (!TIMEOUT.equals(null)) clientSocket.setSoTimeout(TIMEOUT);
+        // If (!TIMEOUT.equals(null)) clientSocket.setSoTimeout(TIMEOUT);
 
-            OutputStream output = clientSocket.getOutputStream();
-            //Sends the data to the server
-            output.write(toServerBytes);
-            output.flush();
+        OutputStream output = clientSocket.getOutputStream();
+        // Sends the data to the server
+        output.write(toServerBytes);
+        output.flush();
 
-            //After sending data shutdown is checked and if it's not null the client wil no longer send any data but it can receive data.
-            if (SHUTDOWN == true) {
-                clientSocket.shutdownOutput();
-            }
-
-            InputStream input = clientSocket.getInputStream();
-            int bytesRead;
-
-            try {
-                if (TIMEOUT != null) {
-                    clientSocket.setSoTimeout(TIMEOUT);
-                }
-
-                //reads the input from the server's input stream and writes it to the
-                //dynamic array.
-                while ((bytesRead = input.read(fromServerBuffer)) != -1) {
-                    byteArrayOutputStream.write(fromServerBuffer, 0, bytesRead);
-                    if(LIMIT != null && byteArrayOutputStream.size() >= LIMIT)
-                        break;
-                }
-            } catch(SocketTimeoutException e){
-
-            } finally{
-                input.close();
-                output.close();
-                clientSocket.close();
-            }
-
+        // After sending data shutdown is checked and if it's not null the client wil no
+        // longer send any data but it can receive data.
+        if (SHUTDOWN == true) {
+            clientSocket.shutdownOutput();
         }
 
+        InputStream input = clientSocket.getInputStream();
+        int bytesRead;
+
+        // try {
+        if (TIMEOUT != null) {
+            clientSocket.setSoTimeout(TIMEOUT);
+        }
+
+        // reads the input from the server's input stream and writes it to the
+        // dynamic array.
+        while ((bytesRead = input.read(fromServerBuffer)) != -1) {
+            byteArrayOutputStream.write(fromServerBuffer, 0, bytesRead);
+            if (LIMIT != null && byteArrayOutputStream.size() >= LIMIT)
+                break;
+        }
+        // } catch (SocketTimeoutException e) {
+
+        // } finally {
+        // input.close();
+        // output.close();
+        // clientSocket.close();
+        // }
+
+        // }
         return byteArrayOutputStream.toByteArray();
     }
 }
